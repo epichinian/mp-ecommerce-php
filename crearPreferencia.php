caa4a443635531d3c27584c7b75d2a53a8ef10e9
Edgardo  git config --global user.name Edgardo <edgardopichinian@gmail.com>
2020-04-25 13:37:44 -0300

    Fix AccessToken

--- crearPreferencia.php
+++ crearPreferencia.php
@@ -6,8 +6,7 @@
 	$basedir = 'https://' . dirname($_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
 
 	// Agrega credenciales
-	MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-46948
-	5398');
+	MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 	// Crea un objeto de preferencia
 	$preference = new MercadoPago\Preference();
