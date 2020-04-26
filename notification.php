30ea6120880d2ef63decf80270f09fb282051c24
Edgardo  git config --global user.name Edgardo <edgardopichinian@gmail.com>
2020-04-26 17:48:44 -0300

    Finaliza ejercicio

--- notification.php
+++ notification.php
@@ -1,15 +1,5 @@
 <?php
-	require ('mercadopago.php');
-	
-	$mp = new MP ("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-
-	$json_event = file_get_contents('php://input');
-	$event = json_decode($json_event);
-
-	$_GET['event'] = $event;
-
-	if ($event->type == 'payment'){
-		$payment_info = $mp->get('/v1/payments/'.$event->data->id);
-		var_dump($payment_info);
-		$_GET['payment'] = $payment_info;
-	}
+	$data = file_get_contents('php://input');
+	$f = fopen('raw.txt', 'a');
+	fwrite($f, $data);
+	fclose($f);
